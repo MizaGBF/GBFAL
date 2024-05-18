f7d885c2e6e0421a4514f93c1f8a09200af75e9c
MizaGBF <backupgbf1@gmail.com>
2024-05-18 12:20:17 +0200

    updated npc detection

--- updater.py
+++ updater.py
@@ -187,6 +187,7 @@ class Updater():
     # scene string
     SCENE_BASE = ["", "_a", "_b", "_c", "_nalhe", "_school", "_astral", "_battle", "_muffler", "_face", "_mask", "_halfmask", "_girl", "_town", "_2022", "_2023", "_2024"]
     SCENE_EXPRESSIONS = ["", "_a", "_b", "_c", "_d", "_e", "_f", "_g", "_up", "_laugh", "_laugh2", "_laugh3", "_laugh4", "_laugh5", "_laugh6", "_laugh7", "_laugh8", "_laugh9", "_wink", "_wink2", "_shout", "_shout2", "_shout3", "_sad", "_sad2",  "_sad3","_angry", "_angry2", "_angry3", "_cry", "_cry2", "_painful", "_painful2", "_shadow", "_shadow2", "_shadow3", "_light", "_close", "_serious", "_serious2", "_serious3", "_serious4", "_serious5", "_serious6", "_serious7", "_serious8", "_serious9", "_serious10", "_serious11", "_surprise", "_surprise2", "_think", "_think2", "_think3", "_think4", "_think5", "_serious", "_serious2", "_mood", "_mood2", "_mood3", "_ecstasy", "_ecstasy2", "_suddenly", "_suddenly2", "_speed2", "_shy", "_shy2", "_weak", "_weak2", "_sleepy", "_open", "_bad", "_bad2", "_amaze", "_amaze2", "_amezed", "_joy", "_joy2", "_pride", "_pride2", "_intrigue", "_intrigue2", "_motivation", "_melancholy", "_concentration", "_weapon", "_letter", "_child1", "_child2", "_eternals", "_eternals2", "_gesu", "_gesu2", "_stump", "_stump2", "_doya", "_2022", "_2023", "_2024", "_two", "_three", "_ef", "_ef_left", "_ef_right", "_body", "_front", "_head", "_up_head", "_foot", "_back", "_left", "_right", "_eyeline"]
+    SCENE_CHECK = list(set(SCENE_BASE + SCENE_EXPRESSIONS))
     SCENE_VARIATIONS = ["", "_a", "_b", "_speed", "_up", "_up2", "_up3", "_up4", "_down", "_shadow", "_shadow2", "_shadow3", "_light", "_up_light", "_blood", "_up_blood"]
     SCENE_VARIATIONS_SET = set(SCENE_VARIATIONS)
     SCENE_SPECIAL = ["_light_heart", "_jewel", "_jewel2", "_thug", "_uncontroll", "_narrator", "_birthday", "_birthday1", "_birthday2", "_birthday3", "_birthday4", "_birthday5", "_valentine", "_valentine2", "_valentine3", "_white", "_whiteday", "_whiteday1", "_whiteday2", "_whiteday3"]
@@ -1585,7 +1586,7 @@ class Updater():
                     if id.startswith("305"): return False # don't continue for special npcs
                 if not exist:
                     # base scene
-                    for f in self.SCENE_BASE:
+                    for f in self.SCENE_CHECK:
                         try:
                             if f not in data[self.NPC_SCENE]:
                                 if (await self.multi_head_nx([self.IMG + "sp/quest/scene/character/body/{}{}.png".format(id, f), self.IMG + "sp/raid/navi_face/{}{}.png".format(id, f)])) is not None:
