0f3fe52e68e06be00c65d56cd3a0c6cd74c383b4
MizaGBF <backupgbf1@gmail.com>
2024-05-23 18:54:49 +0200

    boss - _v_ order fix

--- js/script.js
+++ js/script.js
@@ -2164,8 +2164,8 @@ function loadAssets_sound(id, sounds)
     let sorted_sound = {"Generic":[]};
     let checks = {
         "": ["Generic", "assets/ui/result_icon/voice.png"],
-        "_v_": ["Standard", "assets/ui/result_icon/voice.png"],
         "_boss_v_": ["Boss", "assets/ui/result_icon/v_boss.png"],
+        "_v_": ["Standard", "assets/ui/result_icon/voice.png"],
         "_navi": ["Story Event", "assets/ui/result_icon/v_event.png"],
         "birthday": ["Happy Birthday", "assets/ui/result_icon/v_birthday.png"],
         "year": ["Happy New Year", "assets/ui/result_icon/art.png"],
