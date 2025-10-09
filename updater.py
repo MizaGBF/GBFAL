8b01e94114ed52f3edb2b62f364b3eff12c08fa1
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2025-10-09 17:08:33 +0200

    Added leader/coop

--- updater.py
+++ updater.py
@@ -3895,7 +3895,7 @@ class Updater():
                             file_estimation += len(v[JOB_ID]) * 5
                             file_estimation += len(v[JOB_ALT]) * 3
                             file_estimation += len(v[JOB_DETAIL]) * 4
-                            file_estimation += len(v[JOB_DETAIL_ALT]) * 15
+                            file_estimation += len(v[JOB_DETAIL_ALT]) * 16
                             file_estimation += len(v[JOB_DETAIL_ALL])
                             
                             file_estimation += len(v[JOB_SPRITE])
