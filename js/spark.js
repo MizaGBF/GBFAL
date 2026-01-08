618ddd466f4784101988caf2b2cc44f897aa133d
ymln <ymln@ymln.name>
2026-01-08 08:09:47 +0200

    Remove highlights on drag end
    They should be always removed in the dragleave handler, so in theory
    this code isn't needed. Adding it just in case dragleave isn't triggered
    for some reason.

--- js/spark.js
+++ js/spark.js
@@ -360,6 +360,11 @@ function handle_dragend(event)
 	if(!is_valid_drag_event(event)) return;
 	drag_id = null;
 	drag_is_spark = null;
+	const sections = document.querySelectorAll(".spark-section");
+	for(let i = 0; i < sections.length; ++i)
+	{
+		sections[i].classList.remove("spark-section-highlight");
+	}
 }
 
 function is_valid_drag_event(event)
