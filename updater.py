1e81904d049da5073165566f5fbccb58615b9c75
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-02-10 20:14:57 +0100

    formatting

--- updater.py
+++ updater.py
@@ -4279,7 +4279,8 @@ class Updater():
     # Start function
     async def start(self : Updater) -> None:
         async with (
-            ClientBuilder().user_agent(
+            ClientBuilder()
+            .user_agent(
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"
             )
             .gzip(True)
