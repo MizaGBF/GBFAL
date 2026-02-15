1e34968c7df23b622889f092942419b2d924ceef
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2026-02-15 23:03:59 +0100

    Added tool to help getting npc names from new events

---
+++ tools/scene/scene_npc_bookmarklet.js
@@ -0,0 +1,36 @@
+javascript:(function () {
+let obj = {files:new Set(), names:{}};
+for(const line of Game.view.scene_view.eventSceneView.scenarioCollection.models)
+{
+	const attr = line.attributes;
+	if(attr && attr.charcter1_big_image && attr.charcter1_big_image != "" && attr.charcter1_big_image.includes("character/body/3"))
+	{
+		const sp = attr.charcter1_big_image.split("/");
+		const file = sp[sp.length - 1].split(".")[0];
+		obj.files.add(file);
+		if(attr.charcter1_name != "null" && attr.charcter1_name != "" && attr.charcter1_name != "???")
+		{
+			const id = file.split("_")[0];
+			if(!(id in obj.names))
+			{
+				obj.names[id] = new Set();
+			}
+			obj.names[id].add(attr.charcter1_name);
+		}
+	}
+}
+obj.files = [...obj.files];
+for(const k of Object.keys(obj.names))
+{
+	obj.names[k] = [...obj.names[k]];
+}
+let copyListener = event => {
+	document.removeEventListener("copy", copyListener, true);
+	event.preventDefault();
+	let clipboardData = event.clipboardData;
+	clipboardData.clearData();
+	clipboardData.setData("text/plain", JSON.stringify(obj));
+};
+document.addEventListener("copy", copyListener, true);
+document.execCommand("copy");
+})();
---
+++ tools/scene/scene_npc_reader.py
@@ -0,0 +1,64 @@
+try:
+    import pyperclip
+except:
+    print("Missing module:")
+    print("pip install pyperclip")
+    exit(0)
+import json
+
+def print_help():
+    print("Commands:")
+    print("\tReturn to add a scene")
+    print("\t'quit' to quit")
+    print("\t'report' to print the data")
+    print("\t'copy' to copy the data")
+
+
+def read(data):
+    try:
+        j = json.loads(pyperclip.paste())
+        for f in j["files"]:
+            sp = f.split("_", 1)
+            if len(sp) == 2:
+                data["suffixes"].add("_" + sp[1])
+            data["ids"].add(sp[0])
+        for k, v in j["names"].items():
+            if k not in data["names"]:
+                data["names"][k] = set()
+            for n in v:
+                data["names"][k].add(n)
+        print("Updated data, type 'report' or 'copy' to retrieve it")
+    except Exception as e:
+        print("Can't read clipboard data:", e)
+        return
+
+def report(data):
+    print(f"# ID list ({len(data['ids'])})")
+    print(" ".join(list(data['ids'])))
+    print(f"# Suffix list ({len(data['suffixes'])}")
+    print(" ".join(list(data['suffixes'])))
+    print(f"# Name list ({len(data['names'])}")
+    for k, v in data['names'].items():
+        print(f"{k}: {", ".join(list(v))}")
+
+def copy(data):
+    try:
+        pyperclip.copy(json.dumps(data, default=list, indent=4, ensure_ascii=False))
+        print("Data has been copied to your clipboard")
+    except:
+        print("An unexpected error occured")
+
+data = {"ids":set(), "suffixes":set(), "names":{}}
+print_help()
+while True:
+    match input().lower():
+        case 'quit'|'q':
+            break
+        case 'help'|'h':
+            print_help()
+        case 'report'|'r':
+            report(data)
+        case 'copy'|'c':
+            copy(data)
+        case _:
+            read(data)
